13310b1a0ac9f1ef2beb21a9c090addf82beb07e
edwinisnow <edwin04@gmail.com>
2019-11-25 00:31:40 +0000

    completed application

---
+++ public/precache-manifest.2ad3af85abbffba2006316616f40cefa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6307ccf0b4a62c82aba214fcb4fa0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18b3b44830077ba4a77c",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "ae09bee35b6f0eee1dea",
+    "url": "/static/js/2.bf748573.chunk.js"
+  },
+  {
+    "revision": "18b3b44830077ba4a77c",
+    "url": "/static/js/main.5fb7d209.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
